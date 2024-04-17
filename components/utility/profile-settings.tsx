fcd201169e9ffd22803b3c9fba870559f422dee3
SSH <HikaFeng@163.com>
2024-04-17 15:22:25 +0800

    Update profile-settings.tsx

--- components/utility/profile-settings.tsx
+++ components/utility/profile-settings.tsx
@@ -750,7 +750,7 @@ export const ProfileSettings: FC<ProfileSettingsProps> = ({ }) => {
                 ) : (
                   <>
                     <Label>OpenRouter</Label>
-                    <div style={{ marginLeft: '1rem', marginRight: '1rem' }}>
+                    <div style={{ marginLeft: '0.5rem', marginRight: '1rem' }}>
                       <Balance
                         limit={openrouterkeyUsage.limit}
                         usage={openrouterkeyUsage.usage}
