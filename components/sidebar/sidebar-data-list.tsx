e83bf2eecc16d96905c58c1da007c3f19a908a47
SSH <HikaFeng@163.com>
2025-02-26 15:34:23 +0800

    优化侧边栏数据列表中的文本格式

--- components/sidebar/sidebar-data-list.tsx
+++ components/sidebar/sidebar-data-list.tsx
@@ -229,8 +229,7 @@ export const SidebarDataList: FC<SidebarDataListProps> = ({
         {data.length === 0 && (
           <div className="flex grow flex-col items-center justify-center">
             <div className=" text-centertext-muted-foreground p-8 text-lg italic">
-              {t("No")}
-              {t(contentType)}.
+              {t("No")} {t(contentType)}.
             </div>
           </div>
         )}
