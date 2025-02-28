374712358252e605a4559dab81345cd307aa91bb
SSH <HikaFeng@163.com>
2025-02-28 12:02:16 +0800

    优化帮助页面的布局，调整样式以改善用户体验

--- app/[locale]/help/page.tsx
+++ app/[locale]/help/page.tsx
@@ -1,6 +1,6 @@
 export default function HelpPage() {
   return (
-    <div className="size-screen flex items-center justify-center">
+    <div className="flex h-screen  items-center justify-center">
       <div className="text-center text-4xl">Help under construction.</div>
     </div>
   )
