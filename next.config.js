02337674456989a35160be8be1de5f39ddc32f38
hikafeng <hikafeng@163.com>
2025-07-17 10:28:29 +0800

    fix: patch swc dependencies in lockfile

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 })
 
 const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === 'development'
+  dest: "public"
 })
 
 module.exports = withBundleAnalyzer(
